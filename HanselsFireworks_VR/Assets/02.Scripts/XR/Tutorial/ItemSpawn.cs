2ea0ebedbd12191c97c46b615b47d11e6f218414
ericj4ngdev <ericjangdev@gmail.com>
2023-11-28 13:17:16 +0900

    spawn item

---
+++ HanselsFireworks_VR/Assets/02.Scripts/XR/Tutorial/ItemSpawn.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VR
+{
+    public class ItemSpawn : MonoBehaviour
+    {
+        public GameObject item;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Spawn()
+        {
+            Debug.Log("Spawn Item");
+            GameObject clone = Instantiate(item, transform);
+            clone.transform.SetParent(null);
+            // gameObject.SetActive(false);
+        }
+    }
+
+}
